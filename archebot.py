dcd1208ebb4aa030e5e2c8b024517608253f6461
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-02-27 16:56:31 +0100

    Create archebot.py
    Main archebot.py file that publishes to the main callbacks of the modules (gps, depth_camera, etc.)

---
+++ archebot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+
+import sys, rospy
+
+from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import Image
+
+import gps, depth_camera
+
+
+def ros_exit() -> None:
+    sys.exit()
+
+
+if "__main__" == __name__:
+    rospy.init_node("archebot", anonymous=True, log_level=rospy.INFO, disable_signals=False)
+    rospy.on_shutdown(ros_exit)
+
+    rospy.Subscriber("/ublox/fix", NavSatFix, gps.main)
+    rospy.Subscriber("/d455_camera/depth/image_rect_raw", Image, depth_camera.main)
+
+    rospy.spin()
