c6543983c93a79b9242a9c449e647831e8afeb28
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-03-06 14:38:08 +0100

    Fixed absolute path in camera.py

--- archebot/src/camera.py
+++ archebot/src/camera.py
@@ -1,18 +1,18 @@
 from cv_bridge import CvBridge
 import cv2
+import os
 
 BRIDGE = CvBridge()
 
-# FULL path needed to work
 # Download from: https://github.com/opencv/opencv/tree/master/data/haarcascades
-face_cascade = cv2.CascadeClassifier('/home/gustav/ros_ws/src/object_detection/scripts/haarcascade_frontalface_default.xml')
+FACE_CASCADE = cv2.CascadeClassifier(os.path.abspath("archebot/include/haarcascade_frontalface_default.xml"))
 
 
 def main(data) -> None:
     cv_img = BRIDGE.imgmsg_to_cv2(data, "passthrough")
 
     gray_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+    faces = FACE_CASCADE.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
 
     print(faces)
     # for (x, y, w, h) in faces:
