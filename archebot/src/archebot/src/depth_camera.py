ab0e11a81c66d84351098b886bdd660faed05e1c
Alexander Scheerder <mr.korting@gmail.com>
2025-04-10 16:54:10 +0100

    From raw depth camera to numpy array that can be viewed using png.
    Co-authored-by: Alexander Scheerder <mr.korting@gmail.com>
    Co-authored-by: Kenji Opdam <kenji.opdam@gmail.com>

--- archebot/src/archebot/src/depth_camera.py
+++ archebot/src/archebot/src/depth_camera.py
@@ -1,2 +1,28 @@
+import traceback
+import numpy as np
+import cv2
+import time
+
 def main(data) -> None:
-    print(data)
+    try:
+        input("op pauze")
+        print(type(data))
+
+        numpdata = cam_to_array(data)
+        
+        cv2.imwrite("/home/pi/test.png", numpdata)
+        print("written")
+        
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        print("exeption AAAAAAAAA")
+        pass
+
+
+def cam_to_array(data) -> np.array:
+    numpdata = np.frombuffer(data.data, dtype=np.uint16)
+    numpdata = (numpdata/max(numpdata)*255).astype("uint8")
+    numpdata = numpdata.reshape(data.height, data.width)
+
+    return numpdata
