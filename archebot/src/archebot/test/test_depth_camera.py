8f02598a051cc1a4684464c1e53817bf8d4d4c68
leorover <leo@rover.com>
2025-04-17 15:13:20 +0100

    added array_to_image() test
    Co-authored-by: Alexander Scheerder <mr.korting@gmail.com>
    Co-authored-by: Kenji Opdam <kenji.opdam@gmail.com>

--- archebot/src/archebot/test/test_depth_camera.py
+++ archebot/src/archebot/test/test_depth_camera.py
@@ -1,5 +1,8 @@
 import random
-from src.depth_camera import cam_to_array
+import numpy as np
+import os
+from src.depth_camera import cam_to_array, array_to_image
+import cv2
 
 class FakeImage:
     def __init__(self):
@@ -13,6 +16,7 @@ def test_cam_to_array():
     
     for height in range(fake_data.height):
         for width in range(fake_data.width):
+            # get number from two 8 bit bytestrings
             raw_pixel = int.from_bytes(
                 (
                     fake_data.data[
@@ -24,3 +28,17 @@ def test_cam_to_array():
             array_pixel = array[height][width]
 
             assert raw_pixel == array_pixel
+
+def test_array_to_image():
+    dummy_data = np.array([[1,2,3],[4,5,6],[7,8,9]])
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(path,"array_to_image_unit_test.png")
+    array_to_image(path,dummy_data)
+    ar = cv2.imread(path)
+
+    # scaled pixels with RGB channels
+    test_data = np.array([[[28, 28, 28], [56, 56, 56], [85, 85, 85]],
+                          [[113, 113, 113], [141, 141, 141], [170, 170, 170]],
+                          [[198, 198, 198], [226, 226, 226], [255, 255, 255]]])
+
+    assert np.array_equal(ar,test_data)
