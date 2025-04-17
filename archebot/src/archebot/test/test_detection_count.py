57153b8c40095d00905a1b1c1560c83ba5ba2327
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-04-17 15:34:14 +0200

    Update test_detection_count.py

--- archebot/src/archebot/test/test_detection_count.py
+++ archebot/src/archebot/test/test_detection_count.py
@@ -15,17 +15,23 @@ def read_count(labelname: str) -> None | int:
     return count
 
 # Test the amount of shards detected with the true amount of shards
-def test_detection_count(imgname: str, labelname: str) -> None:
+def helper_detection_count(imgname: str, labelname: str) -> None:
     # Load image
     img = cv2.imread(imgname)
     assert img is not None, f"Image '{imgname}' could not be loaded"
 
     # Get true count from filename
     true_count = read_count(labelname)
-    assert true_count is not None, f"Labels '{labelname}' could not be loaded"
+    assert true_count is not None, "A valid label file must be given"
 
     # Run detection
     pred_count = len(model.predict(source=img, conf=0.6)[0].boxes)
 
     # Check predicted with true
     assert pred_count == true_count, f"Predicted {pred_count}, but expected {true_count}"
+
+def test_detection_count():
+    helper_detection_count("test_detection_0.jpeg", "test_detection_0.txt")
+    helper_detection_count("test_detection_1.jpeg", "test_detection_1.txt")
+    helper_detection_count("test_detection_2.jpeg", "test_detection_2.txt")
+    helper_detection_count("test_detection_5.jpeg", "test_detection_5.txt")
