8853ebfbcd93f5f83074292dcda8681a3afcba22
Alex Ander <Nooitnooitnietzeggen@outlook.com>
2025-04-17 13:35:43 +0200

    cleaned up code
    Co-authored-by: Alexander Scheerder <mr.korting@gmail.com>
    Co-authored-by: Kenji Opdam <kenji.opdam@gmail.com>

--- archebot/src/archebot/src/depth_camera.py
+++ archebot/src/archebot/src/depth_camera.py
@@ -1,26 +1,34 @@
 import traceback
 import numpy as np
 import cv2
-import time
+import os
 
 def main(data) -> None:
-    try:
-        input("op pauze")
-        print(type(data))
+    path = os.path.dirname(os.path.abspath(__file__))
+    numpdata = cam_to_array(data)
+    path = os.path.join(path,"test.png")
+    array_to_image(path, numpdata)
 
-        numpdata = cam_to_array(data)
-        
-        cv2.imwrite("/home/pi/test.png", numpdata)
-        print("written")
-        
+def array_to_image(path: str, numpdata: np.array) -> None:
+    """
+    Writes camera data to image file
+    Input: path: string, array: numpy array
+    Output: None (image file in directory)
+    """
+    try:
+        cv2.imwrite(path, numpdata)
     except Exception as e:
         print(e)
         print(traceback.format_exc())
-        print("exeption AAAAAAAAA")
-        pass
-
 
 def cam_to_array(data) -> np.array:
+    """
+    Writes camera data to numpy array
+    Input: data:
+    Output: numpdata: np.array
+    """
+    print(type(data))
+    input()
     numpdata = np.frombuffer(data.data, dtype=np.uint16)
     numpdata = (numpdata/max(numpdata)*255).astype("uint8")
     numpdata = numpdata.reshape(data.height, data.width)
