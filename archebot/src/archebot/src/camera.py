3803ce4ef4c77d9359c4a4767280b3cb51b6bbbd
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-04-24 11:17:03 +0200

    Update camera.py

--- archebot/src/archebot/src/camera.py
+++ archebot/src/archebot/src/camera.py
@@ -2,9 +2,8 @@ from ultralytics import YOLO
 import os
 import ros_numpy
 
-
-model = YOLO("../include/best.pt")
-
+cwd = os.path.dirname(os.path.abspath(__file__))
+model = YOLO(os.path.join(cwd, "../include/best.pt"))
 
 def main(data) -> None:
     img = ros_numpy.numpify(data)
