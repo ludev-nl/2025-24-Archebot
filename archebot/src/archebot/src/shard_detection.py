5c858a81f991832e115d45374addec2fcaea8092
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-04-24 16:58:16 +0200

    Shard detection now has a debug switch to output images with bounding boxes and predictions, and also saves the image file with gps coordinates as name when shards are found (so findings can be checked and searched)

--- archebot/src/archebot/src/shard_detection.py
+++ archebot/src/archebot/src/shard_detection.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from ultralytics import YOLO
 import os
 import ros_numpy
@@ -5,25 +6,45 @@ import ros_numpy
 cwd = os.path.dirname(os.path.abspath(__file__))
 model = YOLO(os.path.join(cwd, "../include/model.pt"))
 
-iterations = -1
-found_shard = True
+gps_iter = -1
+camera_iter = -1
+shard_img = None
+
+NDEBUG = False
+if not NDEBUG:
+    result_count = 0
 
 def save_location(data) -> None:
-    global found_shard
-    if not found_shard:
+    global gps_iter
+    gps_iter += 1
+    if gps_iter % 60:
         return
-    found_shard = False
-
-    print(data)
+    global shard_img
+    if shard_img is None:
+        return
+    shard_img.save(f"archebot/{data.longitude},{data.latitude}")
+    shard_img = None
 
 def shard_detection(data) -> None:
-    global iterations
-    iterations += 1
-    if iterations % 60:
+    global camera_iter
+    camera_iter += 1
+    if camera_iter % 60:
         return
-    
-    img = ros_numpy.numpify(data)
-    results = model.predict(img)
-    if 0 != len(results):
-        global found_shard
-        found_shard = True
+    img = ros_numpy.numpify(data)[:,:,::-1]
+    global shard_img
+    if shard_img is None:
+        if NDEBUG:
+            results = model.predict(img, stream=True, verbose=False)
+        else:
+            results = model.predict(img, name="tmp", save=True, stream=True, project="archebot", verbose=True)
+        for result in results:
+            if not NDEBUG:
+                while not os.path.exists("archebot/tmp/image0.jpg"):
+                    pass
+                global result_count
+                os.rename("archebot/tmp/image0.jpg", f"archebot/{result_count}.jpg")
+                os.rmdir("archebot/tmp")
+                result_count += 1
+            if 0 != len(result.boxes):
+                shard_img = Image.fromarray(img)
+                break
