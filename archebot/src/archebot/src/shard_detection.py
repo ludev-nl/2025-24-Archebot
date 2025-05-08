2397b73de6b39c5275e497ba1a322d7025eb2a5a
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-05-08 12:50:10 +0200

    Removed debug code

--- archebot/src/archebot/src/shard_detection.py
+++ archebot/src/archebot/src/shard_detection.py
@@ -4,17 +4,12 @@ import os
 import ros_numpy
 import sqlite3
 
-
 cwd = os.path.dirname(os.path.abspath(__file__))
 model = YOLO(os.path.join(cwd, "../include/model.pt"))
 DB_PATH = os.path.join(cwd, "server/db/database.db")
 gps_iter = -1
 camera_iter = -1
 shard_image = None
-
-NDEBUG = True
-if not NDEBUG:
-    result_count = 0
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
@@ -38,7 +33,7 @@ def save_location(data) -> None:
         image_path_relative = f"server/src/static/{data.longitude},{data.latitude}_{index}.jpg"
         index += 1
     
-    full_path = cwd + "/" + image_path_relative
+    full_path = f"{cwd}/{image_path_relative}"
     shard_image.save(full_path)
     
     # database entry
@@ -55,23 +50,8 @@ def shard_detection(data) -> None:
     global shard_image
     if shard_image is None:
         data = ros_numpy.numpify(data)
-        if NDEBUG:
-            results = model.predict(data, stream=True, verbose=False)
-        else:
-            results = model.predict(data, name="tmp", save=True, stream=True, project="archebot", verbose=True)
+        results = model.predict(data, stream=True, verbose=False)
         for result in results:
-            if not NDEBUG:
-                while not os.path.exists("archebot/tmp/image0.jpg"):
-                    pass
-                global result_count
-                try:
-                    os.makedirs("archebot/debug")
-                    os.rename("archebot/tmp/image0.jpg", f"archebot/debug/{result_count}.jpg")
-                    os.rmdir("archebot/tmp")
-                except Exception:
-                    pass
-                result_count += 1
             if 0 != len(result.boxes):
                 shard_image = Image.fromarray(data)
-                #shard_image = data
                 break
