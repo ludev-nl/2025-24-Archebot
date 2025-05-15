2ba7ed4d5a8910e47e3b464ba9829da9857291f2
TechFury03 <leon.monster@hotmail.com>
2025-05-15 13:53:25 +0200

    Added webserver startup to node initialisation

--- archebot/src/archebot/src/archebot.py
+++ archebot/src/archebot/src/archebot.py
@@ -2,16 +2,35 @@
 #coding=utf-8
 
 from sensor_msgs.msg import Image, NavSatFix
-import sys, rospy, shard_detection
+import sys
+import rospy
+import shard_detection
+import os
+import threading
+
+# Add /server/src to PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), "server", "src"))
+from routes import app
+
+def start_flask():
+    app.run(host="0.0.0.0", port=5000)
 
 def ros_exit() -> None:
     sys.exit()
 
 if "__main__" == __name__:
+    # Start the webserver
+    flask_thread = threading.Thread(target=start_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
+    
+    # Initialize ros node
     rospy.init_node("archebot", anonymous=True, log_level=rospy.INFO, disable_signals=False)
     rospy.on_shutdown(ros_exit)
 
+    # Subscribe on topics
     rospy.Subscriber("/ublox/fix", NavSatFix, shard_detection.save_location)
     rospy.Subscriber("/camera/image_raw", Image, shard_detection.shard_detection)
     
+    # Block untill the node is shutdown
     rospy.spin()
