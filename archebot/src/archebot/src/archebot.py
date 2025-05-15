df6bbec0c502255acf9ec73b6a030254e5ba096e
GuusOomen <84714397+GuusOomen@users.noreply.github.com>
2025-05-15 16:24:36 +0200

    Modify archebot.py to work with only drive.py

--- archebot/src/archebot/src/archebot.py
+++ archebot/src/archebot/src/archebot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
-from sensor_msgs.msg import Image, NavSatFix
-import sys, rospy, shard_detection
+from sensor_msgs.msg import Image, NavSatFix, Imu
+# from leo_msgs.msg import Imu
+import sys, rospy, drive
 
 def ros_exit() -> None:
     sys.exit()
@@ -11,7 +12,7 @@ if "__main__" == __name__:
     rospy.init_node("archebot", anonymous=True, log_level=rospy.INFO, disable_signals=False)
     rospy.on_shutdown(ros_exit)
 
-    rospy.Subscriber("/ublox/fix", NavSatFix, shard_detection.save_location)
-    rospy.Subscriber("/camera/image_raw", Image, shard_detection.shard_detection)
+    rospy.Subscriber("/imu/data", Imu, drive.imu_callback)
+    rospy.Subscriber("/ublox_gps_node/fix", NavSatFix, drive.drive_towards_target)
     
     rospy.spin()
