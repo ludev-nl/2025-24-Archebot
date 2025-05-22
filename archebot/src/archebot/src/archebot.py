6f28cfae748325eb276fb92429b965ad68a9ae43
GuusOomen <84714397+GuusOomen@users.noreply.github.com>
2025-05-22 14:15:39 +0200

    Update archebot.py

--- archebot/src/archebot/src/archebot.py
+++ archebot/src/archebot/src/archebot.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
-#coding=utf-8
+# coding=utf-8
 
+import sys
+import rospy
+from std_msgs.msg import String
 from sensor_msgs.msg import Image, NavSatFix, Imu
-# from leo_msgs.msg import Imu
-import sys, rospy, drive
+from geometry_msgs.msg import Twist
 
-def ros_exit() -> None:
-    sys.exit()
+from gps_logger import log_location
+from object_avoidance import Avoider
+from drive import Driver  # Assumes integrated Driver class with GPS/IMU logic
 
-if "__main__" == __name__:
-    rospy.init_node("archebot", anonymous=True, log_level=rospy.INFO, disable_signals=False)
+def ros_exit():
+    rospy.loginfo("Shutting down ArcheBot node...")
+    sys.exit(0)
+
+def main():
+    rospy.init_node("archebot", anonymous=True, log_level=rospy.INFO)
     rospy.on_shutdown(ros_exit)
 
-    rospy.Subscriber("/imu/data", Imu, drive.imu_callback)
-    rospy.Subscriber("/ublox_gps_node/fix", NavSatFix, drive.drive_towards_target)
+    # Instantiate components
+    avoider = Avoider()
+    driver = Driver()
+
+    # Subscriptions
+    rospy.Subscriber("/d455_camera/depth/image_rect_raw", Image, avoider.initialize)
+    rospy.Subscriber("/d455_camera/depth/image_rect_raw", Image, avoider.detect_object)
+    rospy.Subscriber("/d455_camera/depth/image_rect_raw", Image, driver.drive)
     
+    rospy.Subscriber("object_detection", String, driver.update_object)
+    rospy.Subscriber("/ublox_gps_node/fix", NavSatFix, driver.update_gps)
+    rospy.Subscriber("/imu/data", Imu, driver.update_imu)
+
+    # Start ROS loop
     rospy.spin()
+
+if __name__ == "__main__":
+    main()
